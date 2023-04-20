032a372625c6e78f240e57e14cf921a1741dba12
ToolMan47 <kh88403@gmail.com>
2023-04-20 15:28:17 +0800

    New Feature : Logout with csrf-token
    feat: logout function
    
    - 因為啟動 csrf, 所以 logout 要使用 post form，要帶有 csrf-token，不然會沒辦法過 CsrfFilter

--- src/main/java/com/toolman/ruler/controller/UserController.java
+++ src/main/java/com/toolman/ruler/controller/UserController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller()
-//@Secured("ROLE_USER")
 public class UserController {
     
     @GetMapping("/user")
